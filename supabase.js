a2bebc6397e1c759fee1398f9dc34957555ed17d
earngig01 <earngigofficial@gmail.com>
2026-08-10 03:25:44 +0100

    Initialize Supabase client

--- supabase.js
+++ supabase.js
@@ -1,2 +1,6 @@
 const SUPABASE_URL = "https://ytvtdelwefykeqwztcyo.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_CLTeqgEB6JSQ7Kqq6rQARw_RV31Ccg3";
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
